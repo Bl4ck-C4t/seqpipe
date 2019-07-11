fe712797e6ec87bff2746563595f3694608aeec1
TheShadow88 <stef4oben@abv.bg>
2019-07-11 13:58:28 +0300

    drawing each transcript with  it's name

--- GeneParser.py
+++ GeneParser.py
@@ -1,6 +1,7 @@
 from sys import argv
 import re
-
+import matplotlib.pyplot as plt
+import matplotlib.patches as patch
 
 class WrongFormat(RuntimeError):
     pass
@@ -13,7 +14,7 @@ entries_reg = r'(?P<key>\w+)\s+(?P<value>\".+?\")'
 # entries_reg2 = r'(?P<name>\w+)\s+(?P<score>.+|\.)\s+(?P<strand>\w+\-|\+)\s+(?P<thickStart>\d+)\s+(?P<thickEnd>\d+)' \
 #               r'\s+(?P<itemRgb>(\d+,\d+,\d+)|0)\s+(?P<blockCount>\d+)'
 entries_reg2 = r"(.+?)\s+"
-reg3 = r'(?P<bin>\d+)\s+(?P<name>\w+)\s+(?P<chrom>\w+)\s+(?P<sign>\+|\-)\s+(?P<txStart>\d+)\s+(?P<txEnd>\d+)' \
+reg3 = r'(?P<bin>\d+)\s+(?P<name>.+?)\s+(?P<chrom>\w+)\s+(?P<sign>\+|\-)\s+(?P<txStart>\d+)\s+(?P<txEnd>\d+)' \
        r'\s+(?P<cdsStart>\d+)\s+(?P<cdsEnd>\d+)\s+(?P<exonCount>\d+)\s+(?P<exonStarts>.+?)\s+(?P<exonEnds>.+?)' \
        r'\s+(?P<score>\d+)\s+(?P<name2>.+?)\s+(?P<cdsStartStat>\w+)\s+(?P<cdsEndStat>\w+?)\s+(?P<exonFrames>.+)'
 
@@ -32,8 +33,9 @@ class Transcript:
 
 
 class BasicGeneModel:
-    def __init__(self, gene_id):
+    def __init__(self, gene_id, strand):
         self.gene_id = gene_id
+        self.strand = strand
 
     def __str__(self):
         return self.gene_id
@@ -43,15 +45,9 @@ class BasicGeneModel:
 
 
 class GeneModel(BasicGeneModel):
-    def __init__(self, gene_id):
-        super().__init__(gene_id)
+    def __init__(self, gene_id, strand):
+        super().__init__(gene_id, strand)
         self.transcripts = []
-
-    def __str__(self):
-        return self.gene_id
-
-    def __repr__(self):
-        return str(self)
 
 
 class Parser:
@@ -68,7 +64,7 @@ class Parser:
                 try:
                     a = next(g for g in all_genomes if g.gene_id == entries["gene_id"])
                 except StopIteration:
-                    a = GeneModel(entries["gene_id"])
+                    a = GeneModel(entries["gene_id"], entries["sign"])
                     all_genomes.append(a)
                 for key in gene_match.groupdict().keys():
                     entries[key] = gene_match.group(key)
@@ -91,7 +87,7 @@ class Parser:
                 entries = gene_match.group("additional").rsplit("\t")
                 entries = dict(zip(fields, entries))
 
-                a = BasicGeneModel(entries["name"])
+                a = BasicGeneModel(entries["name"], entries["sign"])
                 # print(f"Added new gene {entries['name']}")
                 a.data = entries
                 all_genomes.append(a)
@@ -104,12 +100,14 @@ class Parser:
         with open(fname, "r") as f:
             for line in f:
                 gene_match = re.search(reg3, line)
+                if line.startswith("#bin"):
+                    continue
                 if gene_match is None:
                     raise WrongFormat("Wrong file format given")
                 try:
                     a = next(g for g in all_genomes if g.gene_id == gene_match.group("name2"))
                 except StopIteration:
-                    a = GeneModel(gene_match.group("name2"))
+                    a = GeneModel(gene_match.group("name2"), gene_match.group("sign"))
                     all_genomes.append(a)
                 trans = Transcript(gene_match.groupdict())
                 a.transcripts.append(trans)
@@ -124,15 +122,40 @@ class Parser:
 class Translator:
     @staticmethod
     def full(fname, genes):
-        with open(fname, "r") as f:
+        with open(fname, "w") as f:
             for gene in genes:
                 for transcript in gene.transcripts:
                     ent = transcript.additional
-                    f.write(f"{ent['bin']} {ent['name']} {ent['chrom']} {ent['sign']} {ent['txStart']} {ent['txEnd']} "
-                            f"{ent['cdsStart']} {ent['cdsEnd']} {ent['exonCount']} {ent['exonStarts']} "
-                            f"{ent['exonEnds']} {ent['score']} {ent['name2']} {ent['cdsStartStat']} "
-                            f"{ent['cdsEndStat']} {ent['exonFrames']}\n")
+                    f.write(
+                        f"{ent['bin']}\t{ent['name']}\t{ent['chrom']}\t{ent['sign']}\t{ent['txStart']}\t{ent['txEnd']} "
+                        f"\t{ent['cdsStart']}\t{ent['cdsEnd']}\t{ent['exonCount']}\t{ent['exonStarts']} "
+                        f"\t{ent['exonEnds']}\t{ent['score']}\t{ent['name2']}\t{ent['cdsStartStat']} "
+                        f"\t{ent['cdsEndStat']}\t{ent['exonFrames']}\n")
         print(f"Object written to file '{fname}'")
+
+
+class Visualizer:
+    @staticmethod
+    def full(genomes):
+        genome: GeneModel = genomes[0]
+
+        strand = genome.transcripts[0].additional["sign"]
+        # plt.figure(1)
+        for trans, y in zip(genome.transcripts, reversed(range(10))):
+            trans: Transcript
+            y /= 10
+            # plt.arrow(0, y, trans.additional["cdsEnd"], y)
+            if strand == "-":
+                plt.arrow(0, y, int(trans.additional["txEnd"]) / 100000000 + 0.6, 0, width=0.01)
+                plt.text(int(trans.additional["txEnd"]) / 100000000 + 0.6, y + 0.04, trans.additional["name"])
+            for start, end in zip(trans.additional["exonStarts"].split(",")[:-1],
+                                  trans.additional["exonEnds"].split(",")[:-1]):
+                pass
+                pass
+                plt.axes().add_patch(patch.Rectangle((int(start) / 100000000 + 0.1, y - 0.04),
+                                                     (int(end) / 100000000 - int(start) / 100000000) * 3000, 0.08))
+                # plt.axes().add_patch(patch.Rectangle((0.2, 0.4), 0.3, 0.3))
+        plt.show()
 
 
 if __name__ == '__main__':
@@ -144,8 +167,9 @@ if __name__ == '__main__':
         elif extension == "bed":
             all_genomes = Parser.bed(argv[1])
         elif extension == "full":
-            all_genomes == Parser.full(argv[1])
-            Translator.full("genes.full", all_genomes)
+            all_genomes = Parser.full(argv[1])
+            # Translator.full("genes.full", all_genomes)
+            Visualizer.full(all_genomes)
         all_genomes = set(all_genomes)
 
         print(f"All genomes read: {', '.join([str(x) for x in all_genomes])}")
